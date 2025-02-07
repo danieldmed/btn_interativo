ed7a3870c5ff75607c32b8325064492ab204269e
Daniel Duarte Medeiros <duartedaniel.contato@gmail.com>
2025-02-07 20:17:24 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function alerta(){
+    var nome = prompt("Olá, digite o seu nome:");
+    alert("Seja bem-vindo " + nome + "!");
+}
+
+
+//window.alert("Olá, seja bem-vindo a página Botão Interativo!");
