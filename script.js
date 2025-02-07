e31cbd76a43b08b0d0366bc41a145158212de11f
Daniel Duarte Medeiros <duartedaniel.contato@gmail.com>
2025-02-07 20:17:57 -0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-function alerta(){
-    var nome = prompt("Olá, digite o seu nome:");
-    alert("Seja bem-vindo " + nome + "!");
-}
-
-
-//window.alert("Olá, seja bem-vindo a página Botão Interativo!");
